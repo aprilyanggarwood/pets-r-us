4b0a921c5350b113d58e78f6fad3283c808388c7
aprilyanggarwood <yangyyj822@hotmail.com>
2022-06-21 21:05:22 -0700

    route set up

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
+// Import
+const path = require("path");
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Static Files
+app.use(express.static("public"));
+
+app.use("/styles", express.static(__dirname + "public/styles"));
+
+// app.use('/js', express.static(__dirname + 'public/js'))
+app.use("/images", express.static(__dirname + "public/images"));
+
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.sendFile(path.join(__dirname, "/views/index.html"));
 });
 
-app.get("/groom.html", (req, res) => {
-  res.send("Hello World!");
+app.get("/grooming", (req, res) => {
+  res.sendFile(path.join(__dirname, "/views/grooming.html"));
 });
+// app.get("/", (req, res) => {
+//   res.send("Hello World!");
+// });
 
+// app.get("/groom.html", (req, res) => {
+//   res.send("Hello World!");
+// });
+
+// Listen on port 3000
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
 });
