3d3d81806c3e60aca7a353f3061793629bb4b24a
aprilyanggarwood <yangyyj822@hotmail.com>
2022-07-06 15:00:56 -0700

    fixed registor file issues

--- models/user.js
+++ models/user.js
@@ -4,10 +4,10 @@ const passportLocalMongoose = require("passport-local-mongoose");
 
 const UserSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
+  // password: { type: String, required: true },
+  password: { type: String },
   email: { type: String, required: true, lowercase: true },
-  createdAt: { type: Date, default: () => Date.now, immutable: true },
-  updatedAt: { type: Date, default: () => Date.now, immutable: true },
+  createdAt: { type: Date, default: Date.now },
 });
 
 UserSchema.plugin(passportLocalMongoose);
